4ca19b48b8d7a153668bae217346fc543f9ff7f3
Bruno Darshan <brunodarshan@gmail.com>
2019-07-01 07:45:25 -0300

    include custom size and color active

--- lib/components/shareds/indicator.dart
+++ lib/components/shareds/indicator.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 
 class Indicator extends StatelessWidget {
   final isActive;
+  final size;
+  final colorActive;
 
-  const Indicator({Key key, this.isActive}) : super(key: key);
+  const Indicator({Key key, this.isActive, this.size = 8.0, this.colorActive = null}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
     final opacity = (isActive) ? 0.5 : 0.3;
+    final color = (isActive && colorActive != null) ? colorActive : Color.fromRGBO(0, 0, 0, opacity);
     return Container(
         margin: EdgeInsets.all(4.0),
-        width: 10.0, 
-        height: 10.0,
+        width: size, 
+        height: size,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(4.0)),
-          color: Color.fromRGBO(0, 0, 0, opacity),
+          color: color,
         ),
         );
   }
